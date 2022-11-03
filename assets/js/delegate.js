b69dd2fb9a4128ce6a73c0c92630884f63441346
Dev4Crypto <92862036+Dev4Crypto@users.noreply.github.com>
2022-11-03 20:58:12 +0530

    Update delegate.js

--- assets/js/delegate.js
+++ assets/js/delegate.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
                         if(hours<10){ hours = '0' + hours; }
                         if(minutes<10){ minutes = '0' + minutes;  }
                         if(seconds<10){ seconds = '0' + seconds ; }            
-                        timestamp = (d.getFullYear() + '-' +months + '-' + d.getDate() +  ' ' + hours + ':'+ minutes + ':' + seconds;
+                        timestamp = d.getFullYear() + '-' +months + '-' + d.getDate() +  ' ' + hours + ':'+ minutes + ':' + seconds;
                         var userWallet = element.topics[1];
                         userWallet = '0x'+userWallet.substr(26);
                         userWallet = getUserAddress(userWallet);
