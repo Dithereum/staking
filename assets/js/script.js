c685c44a884002af16cb718a04ead7e5245d0975
Dev4Crypto <92862036+Dev4Crypto@users.noreply.github.com>
2022-11-03 20:39:16 +0530

    stake with amount

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 apiURL = 'https://api.dithereum.io/getallvalidators';
-
     async function init(){
         contractInstance = new myweb3.eth.Contract(ABI, contractAddress, {
             from: myAccountAddress, // default from address
@@ -27,7 +26,7 @@ apiURL = 'https://api.dithereum.io/getallvalidators';
                                 '<td>'+comission+'</td>'+
                                 '<td>'+apr+'</td>'+
                                 '<td><span class="badge bg-success">'+status+'</span></td>'+
-                                '<td><button class="btnStake btn btn-outline-primary btn-sm">Stake</button></td>'+
+                                '<td><button data-wallet="'+validatorWalletAddress+'" class="btnStake btn btn-outline-primary btn-sm">Stake</button></td>'+
                             '</tr>';
                 });
                 $("#validatorsTable").html(tblData);
@@ -36,74 +35,66 @@ apiURL = 'https://api.dithereum.io/getallvalidators';
     }
     setTimeout(init,1000);
 
-    $('.btnStake').click(async function(){
+    $( document ).on( "click", ".btnStake", function() {
         const validatorAddress = $(this).data('wallet');
-        if (window.ethereum) {
-            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-                if (accounts == null || accounts.length == 0) {
-                    console.log("NO ACCOUNT CONNECTED");
-                    alert('Please connect with Metamask');
-                    return false;
-                } else {
-                    if (myAccountAddress != accounts[0]) {
-                        myAccountAddress = accounts[0];                    
-                    }                   
-                 }
-            
-            var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-                if (isMobile && window.ethereum.isMetaMask==true){
-                         const accounts_ = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                         if (accounts_ == null || accounts_.length == 0) {
-                            console.log("NO ACCOUNT CONNECTED");
-                            alert('Please connect with Metamask');
-                            return false;
-                        } else {
-                            if (myAccountAddress != accounts_[0]) {
-                                myAccountAddress = accounts_[0];                    
-                            }
-                    }   
-            }
-            
-            var gasLimit = 300000;
-            gasLimit = gasLimit.toString();
-            var payableAmount = 32 * 1e18;
-           // console.log(myAccountAddress)
-            //const maxValidators = await contractInstance.methods.stake('0xBFb9B248D0e735032a70826572f79381dDC7F0De').send();
-            const web3GasPrice = await myweb3.eth.getGasPrice();
-            var result = await contractInstance.methods.stake(validatorAddress).send({
-                from: myAccountAddress,
-                to: contractAddress,
-                //gasPrice: 100,
-                gasPrice: web3GasPrice,
-                gasLimit: gasLimit,
-                value : payableAmount,       
-            });
-    
-            if(result){
-                alertify.alert("Success !", "Successfully Staked.", "success");
-            }
+        alertify.prompt( 'Staking Dithereum', 'Enter Value', ''
+        ,async function(evt, value) {
+          if($.isNumeric(value)==false){alert('Please enter Valid Amount.'); return false;}
+            if (window.ethereum) {
+                const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+                    if (accounts == null || accounts.length == 0) {
+                        console.log("NO ACCOUNT CONNECTED");
+                        alert('Please connect with Metamask');
+                        return false;
+                    } else {
+                        if (myAccountAddress != accounts[0]) {
+                            myAccountAddress = accounts[0];                    
+                        }                   
+                    }
+                
+                var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+                    if (isMobile && window.ethereum.isMetaMask==true){
+                            const accounts_ = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                            if (accounts_ == null || accounts_.length == 0) {
+                                console.log("NO ACCOUNT CONNECTED");
+                                alert('Please connect with Metamask');
+                                return false;
+                            } else {
+                                if (myAccountAddress != accounts_[0]) {
+                                    myAccountAddress = accounts_[0];                    
+                                }
+                        }   
+                }
+                
+                var gasLimit = 300000;
+                gasLimit = gasLimit.toString();
+                var payableAmount = value * 1e18;
+                payableAmount = logEtoLongNumber(payableAmount);
+            // console.log(myAccountAddress)
+                //const maxValidators = await contractInstance.methods.stake('0xBFb9B248D0e735032a70826572f79381dDC7F0De').send();
+                const web3GasPrice = await myweb3.eth.getGasPrice();
+                var result = await contractInstance.methods.stake(validatorAddress).send({
+                    from: myAccountAddress,
+                    to: contractAddress,
+                    //gasPrice: 100,
+                    gasPrice: web3GasPrice,
+                    gasLimit: gasLimit,
+                    value : payableAmount,       
+                });
+        
+                if(result){
+                    alertify.alert("Success !", "Successfully Staked.", "success");
+                }
+
+            } 
 
         }
+        , function() { alertify.error('Cancel') });
+        
     });
 });
 
 var button = document.getElementById("addDTHNetwork");
 button.addEventListener("click",function(e){
-     if(window.ethereum) {
-               
-        window.ethereum.request({method: 'eth_requestAccounts'})
-        window.ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [{chainId: '0x22', //testnet '0x61',
-                chainName: "Dithereum Testnet",
-                nativeCurrency: {
-                name: "Binance Chain",
-                symbol: "DTH",
-                decimals: 18
-                },
-                rpcUrls: ['https://node-testnet.dithereum.io/'],     blockExplorerUrls: ['https://testnet.dthscan.io/']                    
-            }]
-        })
-            
-      }
+    addNetwork();
 },false);    
