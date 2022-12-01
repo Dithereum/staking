b47c30e7137ae87917b4695ad9290237fcb9f1f3
Dev4Crypto <92862036+Dev4Crypto@users.noreply.github.com>
2022-12-01 16:50:48 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,68 @@
 $(document).ready(function(){
-var validator_URL = apiURL+'getallvalidators';
+
     async function init(){
         contractInstance = new myweb3.eth.Contract(ABI, contractAddress, {
             from: myAccountAddress, // default from address
         });
-        const fetchResponse =  await fetch(validator_URL);
-        const edata = await fetchResponse.json();   
-        var tblData = "";
+        viewContractInstance = new myweb3.eth.Contract(dataViewABI, dataViewContractAddress, {
+            from: myAccountAddress, // default from address
+        });
+        //validators Table data
+        const getAllValidatorInfo = await viewContractInstance.methods.getAllValidatorInfo().call();
+        var totalValidatorCount = getAllValidatorInfo.totalValidatorCount;
+        var totalStakedCoins = getAllValidatorInfo.totalStakedCoins;
+        $('#validatorsText').html(totalValidatorCount+'/21');
+        $('#bondedTokens').html(totalStakedCoins/Math.pow(10,decimals));
         
-            const total_validators = edata.activeValidators;
-            const validators = edata.validators_list;
-            $('#validatorsText').html(validators.length+'/21');
-            $('#bondedTokens').html(edata.bondedtokens);
-            if(validators.length>0){
-                validators.forEach(element => {
-                    const apr = element.validatorAPR;
-                    const comission =element.validatorCommission;
-                    const status = element.status;
-                    const validator = element.validatorName;
-                    const voting_power = element.votingpower;
-                    const validatorWalletAddress = element.validatorWalletAddress
-                    tblData+='<tr>'+
-                                '<td><a href="delegate.html?address='+validatorWalletAddress+'" class=""><img src="https://raw.githubusercontent.com/binance-chain/validator-directory/main/validators/bva1z0g0cg8dkgczr6r8t6khva3srn5mwj8w5tlu7h/logo.png" class="gYNxxe" style="width: 24px;"> '+validator+'</a></td>'+
-                                '<td>'+voting_power+'</td>'+
-                                '<td>'+comission+'</td>'+
-//                                 '<td>'+apr+'</td>'+
-                                '<td><span class="badge bg-success">'+status+'</span></td>'+
-                                '<td><a href="delegate.html?address='+validatorWalletAddress+'" class=""><button data-wallet="'+validatorWalletAddress+'" class="btn btn-outline-primary btn-sm">Stake</button></a></td>'+
-                            '</tr>';
-                });
-                $("#validatorsTable").html(tblData);
+        var tblData = "";
+        for(var i=0;i<totalValidatorCount;i++){
+            var staked = getAllValidatorInfo[4][i];
+            staked = staked/Math.pow(10,decimals);
+            staked = staked.toFixed(4);
+            var status = getAllValidatorInfo[3][i];
+            var validatorName =  getAllValidatorInfo[5][i];
+            if(validatorName==""){
+                validatorName = getAllValidatorInfo[2][i];
             }
+            var validatorWeb =  getAllValidatorInfo[6][i];
+            if(status==2){
+                status = '<span class="badge bg-success">Active</span>';
+            }else{
+                status = '<span class="badge bg-danger">Inactive</span>';
+            }
+            tblData+='<tr>'+
+                                 '<td><a href="validators.html?address='+getAllValidatorInfo[2][i]+'" class=""><img src="https://raw.githubusercontent.com/binance-chain/validator-directory/main/validators/bva1z0g0cg8dkgczr6r8t6khva3srn5mwj8w5tlu7h/logo.png" class="gYNxxe" style="width: 24px;"> '+validatorName+'</a></td>'+
+                                 '<td>'+validatorWeb+'</td>'+
+                                 '<td>'+staked+'</td>'+
+
+                                 '<td>'+status+'</td>'+
+                                 '<td><a href="validators.html?address='+getAllValidatorInfo[2][i]+'" class=""><button class="btn btn-outline-primary btn-sm">Stake</button></a></td>'+
+                             '</tr>';
+        }
+        $("#validatorsTable").html(tblData);
+        //Master Voters Table data
+        const getAllMasterVotersInfo = await viewContractInstance.methods.getAllMasterVotersInfo().call();
+       
+        var totalMasterVoters = getAllMasterVotersInfo.totalMasterVoters;
+        $('#masterVoters').html(totalMasterVoters);
+        tblData = "";
+        for(var i=0;i<totalMasterVoters;i++){
+            var staked = getAllMasterVotersInfo[3][i];
+            staked = staked/Math.pow(10,decimals);
+            staked = staked.toFixed(4);
+            tblData+='<tr>'+
+                                 '<td><a href="masters.html?address='+getAllMasterVotersInfo[2][i]+'&validator='+getAllMasterVotersInfo[1][i]+'" class="">'+getAllMasterVotersInfo[2][i]+'</a></td>'+
+                                 '<td><a href="validators.html?address='+getAllMasterVotersInfo[1][i]+'" class="">'+getAllMasterVotersInfo[1][i]+'</a></td>'+
+                                 '<td>'+staked+'</td>'+
+                                 '<td><span class="badge bg-success">Active</span></td>'+
+                                 '<td><a href="masters.html?address='+getAllMasterVotersInfo[2][i]+'" class=""><button class="btn btn-outline-primary btn-sm">Stake</button></a></td>'+
+                             '</tr>';
+        }
+        $("#mastersTable").html(tblData);
+
         
     }
     setTimeout(init,1000);
-
-    $( document ).on( "click", ".btnStake", function() {
-        const validatorAddress = $(this).data('wallet');
-        alertify.prompt( 'Staking Dithereum', 'Enter Value', ''
-        ,async function(evt, value) {
-          if($.isNumeric(value)==false){alert('Please enter Valid Amount.'); return false;}
-            if (window.ethereum) {
-                const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-                    if (accounts == null || accounts.length == 0) {
-                        console.log("NO ACCOUNT CONNECTED");
-                        alert('Please connect with Metamask');
-                        return false;
-                    } else {
-                        if (myAccountAddress != accounts[0]) {
-                            myAccountAddress = accounts[0];                    
-                        }                   
-                    }
-                
-                var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-                    if (isMobile && window.ethereum.isMetaMask==true){
-                            const accounts_ = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                            if (accounts_ == null || accounts_.length == 0) {
-                                console.log("NO ACCOUNT CONNECTED");
-                                alert('Please connect with Metamask');
-                                return false;
-                            } else {
-                                if (myAccountAddress != accounts_[0]) {
-                                    myAccountAddress = accounts_[0];                    
-                                }
-                        }   
-                }
-                
-                var gasLimit = 300000;
-                gasLimit = gasLimit.toString();
-                var payableAmount = value * 1e18;
-                payableAmount = logEtoLongNumber(payableAmount);
-            // console.log(myAccountAddress)
-                //const maxValidators = await contractInstance.methods.stake('0xBFb9B248D0e735032a70826572f79381dDC7F0De').send();
-                const web3GasPrice = await myweb3.eth.getGasPrice();
-                var result = await contractInstance.methods.stake(validatorAddress).send({
-                    from: myAccountAddress,
-                    to: contractAddress,
-                    //gasPrice: 100,
-                    gasPrice: web3GasPrice,
-                    gasLimit: gasLimit,
-                    value : payableAmount,       
-                });
-        
-                if(result){
-                    alertify.alert("Success !", "Successfully Staked.", "success");
-                }
-
-            } 
-
-        }
-        , function() { alertify.error('Cancel') });
-        
-    });
 });
 
 var button = document.getElementById("addDTHNetwork");
