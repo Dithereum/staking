72511be027a00316ed47d033508d97af5ccc9c0e
EtherAuthority <157196115+EtherAuthorityIO@users.noreply.github.com>
2024-02-09 18:39:56 +0530

    Create Proposal_DB.sql

---
+++ Proposal_DB.sql
@@ -0,0 +1,18 @@
+--
+-- Table structure for table `validators`
+--
+
+DROP TABLE IF EXISTS `proposals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proposals` (
+  `pId` varchar(255) DEFAULT NULL,
+  `proposer` varchar(255) DEFAULT NULL,
+  `dst` varchar(255) DEFAULT NULL,   
+  `status` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
